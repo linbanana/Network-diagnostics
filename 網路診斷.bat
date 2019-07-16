e573d7d5af4c21d5e3dce8c326bd3a49e05540f5
linbanana <tommy12345689@yahoo.com.tw>
2019-07-17 00:12:02 +0800

    Add files via upload

--- 網路診斷.bat
+++ 網路診斷.bat
@@ -1,11 +1,11 @@
 @echo off
+:run
 title=巴那那網路診斷程式
 echo 　　　－－－－－－－－－－－－－－
 echo      ｜　　巴那那網路診斷程式　　｜
 echo 　　　－－－－－－－－－－－－－－
 echo 歡迎使用巴那那網路診斷程式！
 echo %date% %time%
-rem mshta vbscript:msgbox("%UserName%您好，請輸入網路測試方式",64,"提示")(window.close)
 echo.
 echo.
 echo.
@@ -19,11 +19,9 @@ set GoogleDNS="8.8.8.8"
 :address
 echo ------------------------------------------------------------------------------
 echo.
-set /p address=是否要輸入診斷的目標位址：1.yes 2.no(使用預設位址)　　
+set /p address=是否要輸入診斷的目標位址：1.yes 2.no(使用預設位址：Google Public DNS IPv4：8.8.8.8)　　
 echo.
-if "%address%"=="1" Goto ipinput
-if "%address%"=="2" Goto log
-
+if "%address%"=="1" (Goto ipinput) else (if "%address%"=="2" (Goto log) else (echo 輸入錯誤!請輸入"1"or"2"... &Goto address))
 
 
 rem log儲存位置設定
@@ -33,7 +31,7 @@ echo ---------------------------------------------------------------------------
 echo.
 set /p log=是否要儲存log檔?(保留掉封包證據~以方便給網路業者查修)1.yes 2.no　
 echo.　
-if "%log%"=="1" (Goto logpath) else (if "%address%"=="1" (Goto how2) else (Goto how))
+if "%log%"=="1" (Goto logpath) else (if "%log%"=="2" (if "%address%"=="1" (Goto how2) else (Goto how)) else (echo 輸入錯誤!請輸入"1"or"2"... &Goto log))
 pause
 
 
@@ -41,9 +39,9 @@ pause
 :logpath
 echo ------------------------------------------------------------------------------
 echo.
-set /p logpath=是否要設定儲存的路徑位置?1.yes 2.no(使用預設位址)　　
+set /p logpath=是否要設定儲存的路徑位置?1.yes 2.no(使用預設位置：桌面)　　
 echo.
-if "%logpath%"=="1" (set /p setpath=請輸入您要儲存的路徑位置：) else (set setpath="C:\Users\%UserName%\Desktop")
+if "%logpath%"=="1" (set /p setpath=請輸入您要儲存的路徑位置：) else (if "%logpath%"=="2" (set setpath="D:\Users\%UserName%\Desktop") else (echo 輸入錯誤!請輸入"1"or"2"... &Goto logpath))
 if "%address%"=="1" (Goto how2) else (Goto how)
 pause
 
@@ -56,7 +54,7 @@ echo ---------------------------------------------------------------------------
 echo.
 set /p how=請輸入您要使用的網路診斷方式：1.ping　2.tracert　　
 echo.
-if "%how%"=="1" (Goto ping) else (Goto tracert)
+if "%how%"=="1" (Goto ping) else (if "%how%"=="2" (Goto tracert) else (echo 輸入錯誤!請輸入"1"or"2"... &Goto how))
 pause
 
 
@@ -67,7 +65,7 @@ rem 自訂診斷網路
 echo ------------------------------------------------------------------------------
 echo.
 set /p where=請輸入您要測試的ip位址：%where%(例如:www.google.com、8.8.4.4)　
-if %where%==%where% Goto log
+if "%where%"=="%where%" Goto log
 
 
 
@@ -75,8 +73,9 @@ if %where%==%where% Goto log
 echo ------------------------------------------------------------------------------
 echo.
 set /p how=請輸入您要使用的網路測試方式：1.ping　2.tracert　　
-if "%how%"=="1" (Goto ping2) else (Goto tracert2)
+if "%how%"=="1" (Goto ping2) else (if "%how%"=="2" (Goto tracert2) else (echo 輸入錯誤!請輸入"1"or"2"... &Goto how2))
 pause
+
 
 
 
@@ -89,15 +88,15 @@ echo 歡迎使用巴那那網路測試程式！
 echo %date% %time%
 echo.
 echo 您選擇的方式是ping，測試位置:%GoogleDNS%(Google DNS)。
+echo.
 echo 系統正在運行中...暫停請按"<Control-C>"
 type nul>%setpath%\%logname%.log
-echo %date% %time%>>%setpath%\%logname%.log
+echo %date%　　%time%>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
-echo      ｜　　巴那那網路診斷程式　　｜>>%setpath%\%logname%.log
+echo      ｜　　巴那那網路診斷程式　　 ｜>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
-if "%log%"=="1" (ping %GoogleDNS% -t >>%setpath%\%logname%.log) else (ping %GoogleDNS% -t)
-pause
-exit
+if "%log%"=="1" (ping %GoogleDNS% -t >>%setpath%\%logname%.log&%setpath%\%logname%.log) else (ping %GoogleDNS% -t)
+Goto End
 
 
 
@@ -110,15 +109,16 @@ echo 歡迎使用巴那那網路測試程式！
 echo %date% %time%
 echo.
 echo 您選擇的方式是tracert，測試位置:%GoogleDNS%(Google DNS)。
+echo.
 echo 系統正在運行中請稍後...
 type nul>%setpath%\%logname%.log
 echo %date% %time%>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
 echo      ｜　　巴那那網路診斷程式　　｜>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
-if "%log%"=="1" (tracert %GoogleDNS% >>%setpath%\%logname%.log) else (tracert %GoogleDNS%)
+if "%log%"=="1" (tracert %GoogleDNS% >>%setpath%\%logname%.log&%setpath%\%logname%.log) else (tracert %GoogleDNS%)
 pause
-exit
+Goto End
 
 
 
@@ -131,15 +131,16 @@ echo 歡迎使用巴那那網路測試程式！
 echo %date% %time%
 echo.
 echo 您選擇的方式是ping，測試位置:%where%。
+echo.
 echo 系統正在運行中...暫停請按"<Control-C>"
 type nul>%setpath%\%logname%.log
 echo %date% %time%>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
 echo      ｜　　巴那那網路診斷程式　　｜>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
-if "%log%"=="1" (ping %where% -t >>%setpath%\%logname%.log) else (ping %where% -t)
+if "%log%"=="1" (ping %where% -t >>%setpath%\%logname%.log&%setpath%\%logname%.log) else (ping %where% -t)
 pause
-exit
+Goto End
 
 
 :tracert2
@@ -151,12 +152,23 @@ echo 歡迎使用巴那那網路測試程式！
 echo %date% %time%
 echo.
 echo 您選擇的方式是tracert，測試位置:%where%。
+echo.
 echo 系統正在運行中請稍後...
 type nul>%setpath%\%logname%.log
 echo %date% %time%>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
 echo      ｜　　巴那那網路診斷程式　　｜>>%setpath%\%logname%.log
 echo 　　　－－－－－－－－－－－－－－>>%setpath%\%logname%.log
-if "%log%"=="1" (tracert %where% >>%setpath%\%logname%.log) else (tracert %where%)
+if "%log%"=="1" (tracert %where% >>%setpath%\%logname%.log&%setpath%\%logname%.log) else (tracert %where%)
 pause
-exit
+Goto End
+
+
+rem 程式是否繼續執行
+
+:End
+echo ------------------------------------------------------------------------------
+echo.
+set /p End=是否結束程式?：1.yes 2.no
+if "%End%"=="1" (exit) else (if "%End%"=="2" (cls&Goto run) else (echo 輸入錯誤!請輸入"1"or"2"... &Goto End))
+pause
